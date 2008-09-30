055364bf8fdfc1c9c7236de731398614ca787659
Stefan Lenselink <Stefan@lenselink.org>
2008-09-30 07:36:08 +0000

    Files for the rc2 release

---
+++ make_release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function do_checkinstall()
+{
+# $1 : Version
+# $2 : PkgArch
+# $3 : PkgRelease
+# $4 : Required packages
+	sudo checkinstall -RD --pkgname=mjs --pkgversion=$1 --pkgarch=$2 --pkgrelease=$3 --pkggroup=Sound --pkgsource=http://www.bolkhuis.nl --pkgaltsource=http://www.bolkhuis.nl/trac/mjs/ --maintainer=beheer@bolkhuis.nl --requires=$4
+}
+echo "********************************************************"
+echo "** Dit is het magische MJS maak debian package script **"
+echo "********************************************************"
+echo -n "Enter Release: "
+read VAR
+sudo rm -Rf /tmp/mjs_build
+sudo svn export http://serv/svn/mjs/trunk /tmp/mjs_build
+cd /tmp/mjs_build 
+sudo ./configure --prefix=/usr
+do_checkinstall 4.0 i386 $VAR libxine1,libncurses5
+sudo ./configure --prefix=/usr --host=amd64 --build=amd64
+do_checkinstall 4.0 amd64 $VAR libxine1,libncurses5
+scp *.deb root@serv:/srv/www/html/mjs/.
+
+#dpkg-packgeinfo etc...
+#sudo rm -Rf /tmp/mjs_build

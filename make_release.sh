f02ad40ad8c115d54bb4b427ef38f06ff65e3396
Peter Moolenaar <petermoolenaar@gmail.com>
2008-10-04 15:06:05 +0000

    Some autoconf thing

--- make_release.sh
+++ make_release.sh
@@ -15,10 +15,16 @@ read VAR
 sudo rm -Rf /tmp/mjs_build
 sudo svn export http://serv/svn/mjs/trunk /tmp/mjs_build
 cd /tmp/mjs_build 
-sudo ./configure --prefix=/usr --host=i386 --build=i386
-do_checkinstall 4.0 i386 $VAR libxine1,libncurses5
-sudo ./configure --prefix=/usr --host=amd64 --build=amd64
-do_checkinstall 4.0 amd64 $VAR libxine1,libncurses5
-scp *.deb root@serv:/srv/www/html/mjs/.
+if [[ "$1" == "i386" ]]
+then 
+	sudo ./configure --prefix=/usr --host=i386 --build=i386
+	do_checkinstall 4.0 i386 $VAR libxine1,libncurses5
+fi
+if [[ "$1" == "amd64" ]]
+then
+	sudo ./configure --prefix=/usr --host=amd64 --build=amd64
+	do_checkinstall 4.0 amd64 $VAR libxine1,libncurses5
+fi
+#scp *.deb root@serv:/srv/www/html/mjs/.
 ssh root@serv "cd /srv/www/html/mjs && dpkg-scanpackages -m . /dev/null | gzip -9c > /srv/www/html/mjs/Packages.gz"
 sudo rm -Rf /tmp/mjs_build

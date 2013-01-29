f3d5f5ddd814ac5f43ab250b55639b526280080c
Stefan Lenselink <Stefan@lenselink.org>
2013-01-29 08:02:23 +0100

    Removed debian package creation, refactoring the package creation part

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mjs package
-#
-0 4	* * *	root	mjs_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mjs):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="mjs" command="/usr/bin/mjs"
